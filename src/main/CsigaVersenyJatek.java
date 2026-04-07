8dd7aa9aeb69f3a3eeabfac6272d039612f41429
Frnc36 <magoriferenc19@gmail.com>
2026-04-07 21:47:33 +0200

    console megjelenites + metodusokra bontva

--- src/main/CsigaVersenyJatek.java
+++ src/main/CsigaVersenyJatek.java
@@ -24,17 +24,65 @@ public class CsigaVersenyJatek {
         cs3 = new Csiga("kék");
         this.jatekos = jatekos;
     }
-
-
+    
+    private void rajzolCsiga(String szin, int tav, String szinezes) {
+        System.out.print(szinezes+szin + ": ");
+        for (int i = 0; i < tav; i++) {
+            System.out.print("-");
+        }
+        System.out.println("🐌"+RESET);
+    }
 
     public void start() {
+        jatekBevezeto();
+
+        jatekos.fogadas();
+
+        System.out.println("\nA verseny kezdődik!\n");
+
+        for (int kor = 1; kor <= KOROK_SZAMA; kor++) {
+            System.out.println("-- " + kor + ".kör" + " --");
+
+            Csiga gyorsitottCsiga = gyositoAd();
+
+            cs1.megy(gyorsitottCsiga == cs1);
+            cs2.megy(gyorsitottCsiga == cs2);
+            cs3.megy(gyorsitottCsiga == cs3);
+
+            rajzolCsiga("piros", cs1.getTav(), PIROS);
+            rajzolCsiga("zöld", cs2.getTav(), ZOLD);
+            rajzolCsiga("kék", cs3.getTav(), KEK);
+            System.out.println();
+        }
+
+    }//start()
+
+    private Csiga gyositoAd() {
+        boolean vanGyorsito = RND.nextInt(100) < GYORSITO_ESELY;
+        Csiga gyorsitottCsiga = null;
+        if (vanGyorsito) {
+            int melyik = RND.nextInt(3);
+            if (melyik == 0) {
+                gyorsitottCsiga = cs1;
+                System.out.println("Gyorsítót kap: piros");
+            } else if (melyik == 1) {
+                gyorsitottCsiga = cs2;
+                System.out.println("Gyorsítót kap: zöld");
+            } else {
+                gyorsitottCsiga = cs3;
+                System.out.println("Gyorsítót kap: kék");
+            }
+        } else {
+            System.out.println("Gyorsítót kap: -");
+        }
+        return gyorsitottCsiga;
+    }
+
+    private void jatekBevezeto() {
         System.out.println("=== CSIGAVERSENY ===");
         System.out.println("3 csiga versenyzik " + KOROK_SZAMA + " körön keresztül.");
         System.out.println("Egy körben egy csiga 0..3 egységet megy.");
         System.out.println("Minden körben " + GYORSITO_ESELY + "% eséllyel valamelyik csiga gyorsítót kap (dupla sebesség).\n");
-
-        jatekos.fogadas();
-
     }
 
 }//class
